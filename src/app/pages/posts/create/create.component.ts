7973ab5310e71ed291b91096150b15733b0b7b1c
Mike Huang <wellwind@mail2000.com.tw>
2023-05-09 19:21:46 +0800

    任務 06：Reactive Forms 繫結表單資料

--- src/app/pages/posts/create/create.component.ts
+++ src/app/pages/posts/create/create.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
 })
 export class CreateComponent {
+  formBuilder = inject(FormBuilder);
 
+  post = this.formBuilder.group({
+    title: this.formBuilder.control(''),
+    description: this.formBuilder.control(''),
+    body: this.formBuilder.control(''),
+    tags: this.formBuilder.array([
+      this.formBuilder.control('Angular'),
+      this.formBuilder.control('HTML'),
+      this.formBuilder.control('CSS'),
+    ]),
+  });
+
+  addTag(tag: string) {
+    if (tag) {
+      this.post.controls.tags.push(this.formBuilder.control(tag));
+    }
+  }
+
+  removeTag(index: number) {
+    this.post.controls.tags.removeAt(index);
+  }
+
+  createPost() {
+    console.log(this.post.value);
+  }
 }
