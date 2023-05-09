64463104da65fc60995d66f0ab0b0949bf2d0f47
Mike Huang <wellwind@mail2000.com.tw>
2023-05-09 18:06:07 +0800

    任務 03：在程式中取得路由參數

--- src/app/pages/posts/post/post.component.ts
+++ src/app/pages/posts/post/post.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PostService } from 'src/app/post.service';
+import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   selector: 'app-post',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  route = inject(ActivatedRoute);
+  postService = inject(PostService);
 
+  article?: Article;
+
+  ngOnInit() {
+    this.route.paramMap
+      .pipe(
+        map((paramMap) => paramMap.get('id') || ''),
+        switchMap((id) => this.postService.getArticle(id))
+      )
+      .subscribe((result) => {
+        this.article = result.article;
+      });
+  }
 }
--- src/app/pages/posts/posts/posts.component.ts
+++ src/app/pages/posts/posts/posts.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PostService } from 'src/app/post.service';
+import { Article } from 'src/app/interfaces/article';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  postService = inject(PostService);
 
+  articles: Article[] = [];
+
+  ngOnInit() {
+    this.postService.getArticles().subscribe((result) => {
+      this.articles = result.articles;
+    });
+  }
 }
